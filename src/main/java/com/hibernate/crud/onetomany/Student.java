5641d928878e60f355b41d0eb90b22c17cea3c90
yella <yella@LAPTOP-Q9ML3J2S>
2022-04-14 19:56:49 +0530

    student java

---
+++ src/main/java/com/hibernate/crud/onetomany/Student.java
@@ -0,0 +1,33 @@
+package com.hibernate.crud.onetomany;
+
+import java.util.Set;
+
+public class Student{
+	private long student_id;
+	private String student_roll_no;
+	private Set studentFees;
+	
+	public Set getStudentFees() {
+		return studentFees;
+	}
+	public void setStudentFees(Set studentFees) {
+		this.studentFees = studentFees;
+	}
+	public long getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(long student_id) {
+		this.student_id = student_id;
+	}
+	public String getStudent_roll_no() {
+		return student_roll_no;
+	}
+	public void setStudent_roll_no(String student_roll_no) {
+		this.student_roll_no = student_roll_no;
+	}
+	
+}
+
+
+
+
